8f396c70f7d561bf824d5b06b9a77f57457c8ebb
Michael Mutert <michael.mutert@rwth-aachen.de>
2018-12-22 12:08:09 +0100

    Initial commit List is working with initial features

---
+++ src/main.elm
@@ -0,0 +1,156 @@
+module Main exposing (Model, Msg(..), TodoEntry, init, main, update, view)
+
+import Browser
+import Html exposing (Attribute, Html, button, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput, onSubmit)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias TodoEntry =
+    { id : Int, text : String, done : Bool }
+
+
+type alias Model =
+    { listName : String
+    , entries : List TodoEntry
+    , maxIndex : Int
+    , currentInput : String
+    , currentDeleteID : String
+    }
+
+
+
+-- Rastet das auch bei dir so aus, wenn ich was tippe, und du woanders bist?
+
+
+init : Model
+init =
+    Model "MyList" [] 0 "" ""
+
+
+
+-- UPDATE
+
+
+type Msg
+    = AddEntry String
+    | AddEntryButtonPressed
+    | EntryMarkedCompleted Int
+    | EntryMarkedNotDone Int
+    | DeleteEntry
+    | DeleteAllEntries
+    | GetText String
+    | UpdateDeleteIdString String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddEntry text ->
+            let
+                newIndex =
+                    model.maxIndex + 1
+            in
+            { model
+                | maxIndex = newIndex
+                , entries = model.entries ++ [ TodoEntry newIndex text False ]
+                , currentInput = ""
+            }
+
+        AddEntryButtonPressed ->
+            update (AddEntry model.currentInput) model
+
+        EntryMarkedCompleted id ->
+            model
+
+        EntryMarkedNotDone id ->
+            model
+
+        DeleteAllEntries ->
+            { model | entries = [] }
+
+        DeleteEntry ->
+            case String.toInt model.currentDeleteID of
+                Just id ->
+                    { model
+                        | entries = List.filter (.id >> (/=) id) model.entries
+                        , currentDeleteID = ""
+                    }
+
+                Nothing ->
+                    { model | currentDeleteID = "" }
+
+        UpdateDeleteIdString id ->
+            { model | currentDeleteID = id }
+
+        GetText text ->
+            { model | currentInput = text }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model.listName
+        , viewInput model.currentInput
+        , viewDeleteItem model.currentDeleteID
+        , viewEntries model
+        , button [ onClick DeleteAllEntries ] [ text "Delete all entries" ]
+        ]
+
+
+{-| An input form for the new todo.
+
+Exploits the browser's understanding of forms and submits.
+Automatically fires the onSubmit event when the submit input is pressed
+as well as when enter is typed in the text input.
+
+-}
+viewInput : String -> Html Msg
+viewInput current =
+    Html.form [ onSubmit AddEntryButtonPressed ]
+        [ input [ placeholder "Enter your new input", value current, onInput GetText ] []
+        , input [ type_ "submit", value "+" ] []
+        ]
+
+
+viewEntries : Model -> Html msg
+viewEntries model =
+    div []
+        (List.map
+            viewEntry
+            model.entries
+        )
+
+
+viewEntry : TodoEntry -> Html msg
+viewEntry todoEntry =
+    div [] [ text (String.fromInt todoEntry.id ++ ": "), text todoEntry.text ]
+
+
+viewDeleteItem : String -> Html Msg
+viewDeleteItem currentID =
+    Html.form [ onSubmit DeleteEntry ]
+        [ input
+            [ placeholder "Enter id of item to delete"
+            , value currentID
+            , onInput UpdateDeleteIdString
+            ]
+            []
+        , input [ type_ "submit", value "Delete specified entry" ] []
+        ]
